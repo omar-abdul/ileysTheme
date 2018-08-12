0bd763bd8656f440722f1eaf5115435cdf72266c
freakshow-ilbuur <ilbuuromar@gmail.com>
2018-08-13 01:27:52 +0300

    responsive navbar

--- functions.php
+++ functions.php
@@ -38,12 +38,14 @@ add_action('after_setup_theme','custom_theme_setup');
 function customTheme_script_enqueue(){
     wp_enqueue_style('bootstrapstyle', get_template_directory_uri()."/css/bootstrap.min.css", array(), "4.1.3", "all" );
     wp_enqueue_style('customstyle', get_template_directory_uri()."/css/ileys.css", array(), "1.0.1", "all" );
-    wp_enqueue_script('mainjs',get_template_directory_uri() . "/js/main.js",array() ,'1.0.0', true);
+
     wp_deregister_script('jquery');
     
-    wp_enqueue_script('jquery', "https://code.jquery.com/jquery-3.3.1.slim.min.js",array() , "3.3.1",true);
+    wp_enqueue_script('jquery', "https://code.jquery.com/jquery-3.3.1.min.js",array() , "3.3.1",true);
     wp_enqueue_script('bootstrapjs',get_template_directory_uri() . "/js/bootstrap.min.js",array('jquery') ,'4.1.3', true);
+    wp_enqueue_script('jqueryui',get_template_directory_uri() . "/js/jquery-ui.min.js",array('jquery') ,'1.12.1', true);
 
+    wp_enqueue_script('mainjs',get_template_directory_uri() . "/js/main.js",array('jquery') ,'1.0.0', true);
 
     wp_enqueue_script('popperjs', "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js",array() , "1.0.0",true);
 }
---
+++ js/main.js
@@ -0,0 +1,7 @@
+(function($){
+
+    $('.icon').click(function(){
+        $('.icon').toggleClass('active');
+        $('.site-nav').toggleClass('site-nav-active',500);
+    })
+})(jQuery)
